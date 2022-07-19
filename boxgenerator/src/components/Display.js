efd8c1ce7ccbe4a323ab8c5c8ed1f29345f47074
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-19 14:47:00 -0700

    Added first component

---
+++ boxgenerator/src/components/Display.js
@@ -0,0 +1,23 @@
+const Display = (props) => {
+
+    const {boxColorArray} = props;
+
+    return (
+    <div>
+        {
+            boxColorArray.map((color, index) => (
+                <div key = {index} style = {{
+                    display: "inline-block",
+                    margin: "20px",
+                    height: "75px",
+                    width: "75px",
+                    backgroundColor: color
+                }}>
+            </div>
+            ))
+        }
+    </div>
+    );
+}
+
+export default Display;
