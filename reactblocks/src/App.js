8cbdb5eba46c51c7cea9d42a1169b03b718d03dd
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-21 12:06:43 -0700

    Adding a new assignment named React Blocks

---
+++ reactblocks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header.js';
+import Navigation from './Components/Navigation';
+import Main from './Components/Main';
+import SubContents from './Components/SubContents';
+import Advertisement from './Components/Advertisement';
+
+function App() {
+  
+}
+
+export default App;
