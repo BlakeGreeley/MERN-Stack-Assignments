f2cf08898fb0a4b7dc87b0a387cea953d829e889
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-23 13:50:36 -0700

    added to assignment

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Form from "./components/Form";
-import Display from ".components/Display"
+import Display from "./components/Display";
 import './App.css';
 
 function App() {
