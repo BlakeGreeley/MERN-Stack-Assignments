e2bebd6b2489ef3964a897adb0ae04566ccc53aa
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-25 13:49:21 -0700

    optional assignment navbar context

--- navbar_context/src/index.js
+++ navbar_context/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import FormWrapper from "./components/FormWrapper";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <Wrapper>
+      <Navbar>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      </Navbar>
+      <FormWrapper/>
+    </Wrapper>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
+
+
