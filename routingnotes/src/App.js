c7e6f1eb96271cb748c9ab7573c31e943152a728
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-29 20:21:11 -0700

    notes from lessons about routing

--- routingnotes/src/App.js
+++ routingnotes/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link 
+} from "react-router-dom";
+    
+const Home = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1 style={{color: "red"}}>Home Component</h1>
+        <Link to={"/about"}>Go to About </Link>
     </div>
   );
 }
-
-export default App;
+    
+const About = (props) => {
+  return (
+    <div>
+        <h1 style={{color: "blue"}}>About Component</h1>
+        <Link to={"/"}>Go Home</Link> 
+    </div>
+  );
+}
+    
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+    
+export default App
