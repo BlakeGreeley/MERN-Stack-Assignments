61b9d8bb57b9ebfc7f882b961956151307c43517
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-31 11:29:33 -0700

    notes on express

--- hello_express/src/server.js
+++ hello_express/src/server.js
@@ -6,4 +6,16 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello World"});
 });
 
+const users = [
+    { firstName: "Reimu",  lastName: "Hakurei"    },
+    { firstName: "Marisa", lastName: "Kirisame"   },
+    { firstName: "Sanae",  lastName: "Kochiya"    },
+    { firstName: "Sakuya", lastName: "Izayoi"     },
+    { firstName: "Momiji", lastName: "Inubashiri" }
+];
+
+app.get("/api/users", (req, res) => {
+    res.json({ users });
+});
+
 app.listen(port, () => console.log('Listening on port: ${port}'));
