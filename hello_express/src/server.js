0de9346a938697e3dfb0074124355f8af1f4a317
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-31 11:56:48 -0700

    notes on express

--- hello_express/src/server.js
+++ hello_express/src/server.js
@@ -13,9 +13,34 @@ const users = [
     { firstName: "Sakuya", lastName: "Izayoi"     },
     { firstName: "Momiji", lastName: "Inubashiri" }
 ];
-
+// getting data
 app.get("/api/users", (req, res) => {
-    res.json({ users });
+    console.log(req.body);
+    users.push(req.body);
+    res.json({ status: "ok" });
+});
+
+// getting by id
+app.get("/api/users/:id", (req, res) => {
+    console.log(req.params.id);
+    res.json( users[req.params.id] );
+});
+
+//updating data
+app.put("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    users[id] = req.body;
+    res.json( {status: "ok" });
+});
+
+//deleting data
+app.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    users.splice(id, 1);
+    res.json( { status: 'ok' });
 });
 
 app.listen(port, () => console.log('Listening on port: ${port}'));
+
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
